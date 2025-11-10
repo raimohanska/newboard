84ff455a90560ddd1fa998189bde6ec0692b22a9
Juha Paananen <juha.paananen@gmail.com>
2025-11-10 10:40:55 +0200

    Show styled components names in DOM

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: true,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
 })
 
